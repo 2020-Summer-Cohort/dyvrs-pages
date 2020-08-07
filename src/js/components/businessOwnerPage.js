23e1af908dceaa1c7b71cd6a0a5b7ab88d4a693b
Shawn Daniels <SGDaniels08@gmail.com>
2020-08-07 10:38:29 -0400

    Business owner page hidden toggling functional

--- src/js/components/businessOwnerPage.js
+++ src/js/components/businessOwnerPage.js
@@ -5,7 +5,26 @@ export{
 
 console.log("above");
 const renderBusinessOwnerPage = () => {
-    document.getElementById ('portal-page').style.display="block";
+    // document.getElementById ('portal-page').style.display="block";
+
+    const portalPage = document.querySelector("#portal-page")
+    const newUserPage = document.querySelector("#new-user-page")
+    const updateUserLogin = document.querySelector("#update-user-login")
+
+    const submitUserButton = document.querySelector(".update-user-button")
+    const newUserButton = document.querySelector(".new-user-button")
+
+    submitUserButton.addEventListener("click", ()=> {
+            portalPage.classList.toggle("hidden")
+            updateUserLogin.classList.toggle("hidden")
+            console.log("Inside event listener")   
+        })
+    newUserButton.addEventListener("click", ()=> {
+            portalPage.classList.toggle("hidden")
+            newUserPage.classList.toggle("hidden")
+            console.log("Inside event listener")   
+        })
+    }
     // if(clickable){
     //     document.getElementById ('new-user-page').style.display="block";
     // } if(x){
@@ -46,7 +65,6 @@ const renderBusinessOwnerPage = () => {
 //     }
 //   }
   console.log("inside");
-}
 function updateOwnerPageFunction() {
     console.log("test")
 }
