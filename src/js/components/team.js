1228408b21254474aea2ed9924dc7eab1ba8f7fe
Jason Orabella <jasonorabella@gmail.com>
2020-08-11 14:23:42 -0400

    worked on fetch team by id

---
+++ src/js/components/team.js
@@ -0,0 +1,31 @@
+import{fetchTeam}from "../fetch-api.js"
+
+export{renderTeam}
+
+console.log("calling Team.js correctly")
+const renderTeam = (id) => {
+    fetchTeam(id).then(team => {
+        const ul = document.createElement('ul');
+        ul.classList.add("team__ul_container")
+
+        console.log("inside RenderTeam")
+
+       team.teamMember.forEach(teamMember => {
+        const li = document.createElement('li');
+        li.classList.add("team__item")
+        li.innerHTML = `<img>
+        <div>${teamMember.name}</div>
+        <div class =pic> </div>
+        <div class = funfact> ${teamMember.funFacts}</div>
+        <a class = linkedin  href="${teamMember.linkedIn} target ="_blank"> lorem ipsum </a>
+        </li>`
+        ul.appendChild(li);
+        console.log("inside line 23");
+    })
+
+    const h2 = document.querySelector('.team__title')
+    h2.innerHTML = category.name
+    h2.appendChild(ul);
+}
+)}
+
--- src/js/html-scripts/teamScripts.js
+++ src/js/html-scripts/teamScripts.js
@@ -1,5 +1,7 @@
 import { renderHeader } from "../components/header.js"
 import { renderFooter } from "../components/footer.js"
+import {renderTeam} from "../components/team.js"
 
 renderHeader();
 renderFooter();
+renderTeam(id);
