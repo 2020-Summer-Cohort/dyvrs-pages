014309a16e5d02508a9b282a50884436d033c647
Christin Banerjee <Christinprice5@gmail.com>
2020-08-11 20:43:59 -0400

    fixed the images in contact page ../images/contactIcons/

--- src/js/components/contactUs.js
+++ src/js/components/contactUs.js
@@ -6,9 +6,9 @@ const renderContactUs = () => {
     const contactContainer = document.createElement("ul");
     contactContainer.classList.add("contact__container");
     contactContainer.innerHTML = `
-        <li class="contact__item"><a href="mailto:"><img src="../images/email.png" alt="Send an email to Dyvrs Pages"></a></li>
-        <li class="contact__item"><a href="http://www.linkedin.com" target="_blank"><img src="../images/linkedin.png" alt="Visit the LinkedIn page for Dyvrs Pages"></a></li>
-        <li class="contact__item"><a href="http://www.instagram.com" target="_blank"><img src="../images/instagram.png" alt="Visit the Facebook page for Dyvrs Pages"></a></li>
+        <li class="contact__item"><a href="mailto:"><img src="../images/contactIcons/email.png" alt="Send an email to Dyvrs Pages"></a></li>
+        <li class="contact__item"><a href="http://www.linkedin.com" target="_blank"><img src="../images/contactIcons/linkedin.png" alt="Visit the LinkedIn page for Dyvrs Pages"></a></li>
+        <li class="contact__item"><a href="http://www.instagram.com" target="_blank"><img src="../images/contactIcons/instagram.png" alt="Visit the Facebook page for Dyvrs Pages"></a></li>
     `;
     contactMain.appendChild(contactContainer);
 }
