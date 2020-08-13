30a292fc1c80fe603b284eeb712810b5dd056756
Christin Banerjee <Christinprice5@gmail.com>
2020-08-12 22:05:41 -0400

    updated color

--- src/js/components/contactUs.js
+++ src/js/components/contactUs.js
@@ -6,8 +6,8 @@ const renderContactUs = () => {
     const contactContainer = document.createElement("ul");
     contactContainer.classList.add("contact__container");
     contactContainer.innerHTML = `
-        <li class="contact__item"><a href="mailto:"><img src="../images/contactIcons/email.png" alt="Send an email to Dyvrs Pages"></a></li>
         <li class="contact__item"><a href="http://www.linkedin.com" target="_blank"><img src="../images/contactIcons/linkedin.png" alt="Visit the LinkedIn page for Dyvrs Pages"></a></li>
+        <li class="contact__item"><div id="email-icon"><a href="mailto:"><img src="../images/contactIcons/email.png" alt="Send an email to Dyvrs Pages"></a></div></li>
         <li class="contact__item"><a href="http://www.instagram.com" target="_blank"><img src="../images/contactIcons/instagram.png" alt="Visit the Facebook page for Dyvrs Pages"></a></li>
     `;
     contactMain.appendChild(contactContainer);
