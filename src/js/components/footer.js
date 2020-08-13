f399bfa0c3fe3f00ea1a1794f58987fb7f9ba313
Christin Banerjee <Christinprice5@gmail.com>
2020-08-13 12:54:38 -0400

    delected contact page

--- src/js/components/footer.js
+++ src/js/components/footer.js
@@ -7,10 +7,11 @@ const renderFooter = () => {
     footer.innerHTML = `
         <nav class="footer__nav">
             <ul class="footer__container">
-                <li class="footer__list-item"><a href="./contactUs.html">Contact Us</a></li>
-                <li class="footer__list-item"><a href="./businessOwnerPage.html">Owner Portal</a></li>
                 <li class="footer__list-item"><a href="./team.html">Team</a></li>
                 <li class="footer__list-item"><a href="./suggestion .html">Suggestions</a></li>
+                <li class="contact__item footer__list-item"><a href="http://www.linkedin.com" target="_blank"><img src="../images/contactIcons/linkedin.png" alt="Visit the LinkedIn page for Dyvrs Pages"></a></li>
+                <li class="contact__item footer__list-item"><div id="email-icon"><a href="mailto:"><img src="../images/contactIcons/email.png" alt="Send an email to Dyvrs Pages"></a></div></li>
+                <li class="contact__item footer__list-item"><a href="http://www.instagram.com" target="_blank"><img src="../images/contactIcons/instagram.png" alt="Visit the Facebook page for Dyvrs Pages"></a></li>
             </ul>
         </nav>
         <div class="footer__copyright">&copy;Dyvrs Pages</div>
